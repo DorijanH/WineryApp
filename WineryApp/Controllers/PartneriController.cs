d89615b71be1d17687ee6ab0e8eb40c3947f92af
dorijan.haspl <dorijan.haspl@fer.hr>
2019-05-22 18:31:34 +0200

    Dodan kontroler i pogledi za Partnere

---
+++ WineryApp/Controllers/PartneriController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WineryApp.Data;
+using WineryApp.Data.Entiteti;
+
+namespace WineryApp.Controllers
+{
+    public class PartneriController : Controller
+    {
+        private readonly WineryAppDbContext _context;
+
+        public PartneriController(WineryAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Partneri
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Partner.ToListAsync());
+        }
+
+        // GET: Partneri/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var partner = await _context.Partner
+                .FirstOrDefaultAsync(m => m.PartnerId == id);
+            if (partner == null)
+            {
+                return NotFound();
+            }
+
+            return View(partner);
+        }
+
+        // GET: Partneri/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Partneri/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PartnerId,ImePartnera,Oib,KontaktBroj,Adresa")] Partner partner)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(partner);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(partner);
+        }
+
+        // GET: Partneri/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var partner = await _context.Partner.FindAsync(id);
+            if (partner == null)
+            {
+                return NotFound();
+            }
+            return View(partner);
+        }
+
+        // POST: Partneri/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PartnerId,ImePartnera,Oib,KontaktBroj,Adresa")] Partner partner)
+        {
+            if (id != partner.PartnerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(partner);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PartnerExists(partner.PartnerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(partner);
+        }
+
+        // GET: Partneri/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var partner = await _context.Partner
+                .FirstOrDefaultAsync(m => m.PartnerId == id);
+            if (partner == null)
+            {
+                return NotFound();
+            }
+
+            return View(partner);
+        }
+
+        // POST: Partneri/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var partner = await _context.Partner.FindAsync(id);
+            _context.Partner.Remove(partner);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PartnerExists(int id)
+        {
+            return _context.Partner.Any(e => e.PartnerId == id);
+        }
+    }
+}
