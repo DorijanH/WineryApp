b4d1f9b339a44774bd72b6601d1975f0190b2564
dorijan.haspl <dorijan.haspl@fer.hr>
2019-05-29 00:30:01 +0200

    Obrisani about page

--- WineryApp/Controllers/HomeController.cs
+++ WineryApp/Controllers/HomeController.cs
@@ -66,11 +66,6 @@ namespace WineryApp.Controllers
             }
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
